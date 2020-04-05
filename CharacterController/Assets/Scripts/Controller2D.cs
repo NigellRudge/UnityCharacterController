c4ed154a658c72c1a13b67b3dc227371cdc7d499
Deyon Rudge <deyon730@gmail.com>
2020-04-05 00:23:15 -0300

    commit # 2
    april 4th 2020 tutorial episode 2 done

--- CharacterController/Assets/Scripts/Controller2D.cs
+++ CharacterController/Assets/Scripts/Controller2D.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public class Controller2D : MonoBehaviour
 {
+    // This value is to of set the position of the raycast of the objects they are fired from
     const float skinWidth = 0.015f;
+    
+    //The amount of rays that are fired from both horizontal and vertical positions
     public int horizonralRayCount = 4;
     public int verticallRayCount = 4;
 
+    public LayerMask collisionMask;
+
+    //T
     float horizontalRaySpacing;
     float verticalRaySpacing;
     BoxCollider2D collider;
@@ -16,18 +22,9 @@ public class Controller2D : MonoBehaviour
     void Start()
     {
         collider = GetComponent<BoxCollider2D>();
-    }
-
-    void Update()
-    {
-        UpdateRaycastOrigins();
         CalculateRaySpacing();
-
-        for(int i = 0; i < verticallRayCount; i++)
-        {
-            Debug.DrawRay(raycastOrigins.bottomLeft + Vector2.right * verticalRaySpacing * i,Vector2.up * -2,Color.red);
-        }
     }
+
     void UpdateRaycastOrigins()
     {
         Bounds bounds = collider.bounds;
@@ -51,6 +48,63 @@ public class Controller2D : MonoBehaviour
         horizontalRaySpacing = bounds.size.y / (horizonralRayCount - 1);
         verticalRaySpacing = bounds.size.x / (verticallRayCount - 1);
     }
+
+    public void Move(Vector3 velocity)
+    {
+        UpdateRaycastOrigins();
+
+        if(velocity.x != 0) { 
+            HorizontalCollisions(ref velocity);
+        }
+
+        if (velocity.y != 0) { 
+            VerticalCollisions(ref velocity);
+        }
+
+        transform.Translate(velocity);
+    }
+
+    void VerticalCollisions(ref Vector3 velocity)
+    {
+        float directionY = Mathf.Sign(velocity.y);
+        float rayLength = Mathf.Abs(velocity.y) + skinWidth;
+        for (int i = 0; i < verticallRayCount; i++)
+        {
+            Vector2 rayOrigin = (directionY == -1) ? raycastOrigins.bottomLeft :raycastOrigins.topLeft;
+            rayOrigin += Vector2.right * (verticalRaySpacing * i + velocity.x);
+
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, collisionMask);
+            Debug.DrawRay(rayOrigin, Vector2.up * directionY * rayLength, Color.red);
+
+            if (hit)
+            {
+                velocity.y = (hit.distance - skinWidth) * directionY;
+                rayLength = hit.distance;
+            }
+        }
+    }
+
+    void HorizontalCollisions(ref Vector3 velocity)
+    {
+        float directionX = Mathf.Sign(velocity.x);
+        float rayLength = Mathf.Abs(velocity.x) + skinWidth;
+        for (int i = 0; i < horizonralRayCount; i++)
+        {
+            Vector2 rayOrigin = (directionX == -1) ? raycastOrigins.bottomLeft :raycastOrigins.bottomRight;
+            rayOrigin += Vector2.up * (horizontalRaySpacing * i);
+
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, collisionMask);
+            Debug.DrawRay(rayOrigin, Vector2.right * directionX * rayLength, Color.red);
+
+            if (hit)
+            {
+                velocity.x = (hit.distance - skinWidth) * directionX;
+                rayLength = hit.distance;
+            }
+        }
+    }
+
+
     struct RaycastOrigins
     {
         public Vector2 topLeft;
--- CharacterController/Assets/Scripts/Player.cs
+++ CharacterController/Assets/Scripts/Player.cs
@@ -5,16 +5,27 @@ using UnityEngine;
 [RequireComponent(typeof(Controller2D))]
 public class Player : MonoBehaviour
 {
-    Controller2D controller2D;
+    // Basic moveSpeed for Player
+    float moveSpeed = 6;
+    // Basic gravity for player
+    float gravity = -20;
+    //velocity of movement for the player
+    Vector3 velocity;
+    Controller2D controller;
     // Start is called before the first frame update
     void Start()
     {
-      controller2D = GetComponent<Controller2D>();  
+      controller = GetComponent<Controller2D>();  
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // get the input from user to move the player
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        velocity.x = input.x * moveSpeed;
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
